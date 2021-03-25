b43c443a7cb97cd51754c1c2e3eae6fdfeedec69
preetisns1292@outlook.com <preetisns1292@outlook.com>
2021-03-25 13:32:00 +0000

    proj commit

---
+++ tools.Web-buzz/Controllers/Web-Tools/CalculatorsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace tools.Web_buzz.Controllers.Web_Tools
+{
+    public class CalculatorsController : Controller
+    {
+        // GET: Calculators
+        public ActionResult AgeDifferenceCalculator()
+        {
+            return View();
+        }
+    }
+}
